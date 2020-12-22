432f7de58c6572ee8f3a854f272604d6da12df54
nordlicht28 <yesenia@moehring-family.de>
2020-12-22 14:07:27 +0100

    functions timesignal and timesbreak

--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -1,18 +1,63 @@
 #include <Arduino.h>
 
-int eingang = A0;
-int sensorWert = 0;
+int input = A0;
+int lastInput;
+
+unsigned long start;
+unsigned long time;
 
 #define dot 100
 #define dash 200
 #define pause 100
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
+void timesignal(){
+  if (input > 512 && lastInput < 512){
+    start = millis();
+    lastInput = input;
+  }
+  
+  if (input <512 && lastInput > 512)
+  {
+    lastInput = input;
+    time = millis() - start; 
+  }
+  
+   if (time == dot)
+  {
+    Serial.print("•");
+  }
+   else if (time == dash)
+   {
+     Serial.print("-");
+   }
+   
+}
+
+void timesbreak(){
+  if (input < 512 && lastInput > 512){
+    start = millis();
+    lastInput = input;
+  }
+  
+  if (input > 512 && lastInput < 512)
+  {
+    lastInput = input;
+    time = millis() - start;
+  }
+  
+}
+
+void timestochar(){
+  
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(eingang);
+   Serial.println(input);
 }
